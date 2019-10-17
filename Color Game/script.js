6ffb5c66ec861a51ded994f791f234f76edc48ce
Stefania Giannopoulou <stefania.gian91@gmail.com>
2019-10-17 10:50:24 +0300

    Update script.js

--- Color Game/script.js
+++ Color Game/script.js
@@ -46,6 +46,8 @@ resetButton.addEventListener("click", function(){
   pickedColor = pickColor();
   //change color display to match picked color
   colorDisplay.textContent = pickedColor;
+  this.textContent = "New Colors";
+  messageDisplay.textContent = " ";
   //change colors of squares
   for(var i = 0; i < squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
